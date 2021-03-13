6433f04cbb0778e6f60f1a76760efeed86d7148f
nadiandnd <ndtaeminnie@gmail.com>
2021-03-13 22:33:08 +0700

    53. Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simply test', 
+    'https://www.kyleecooks.com/wp-content/uploads/2020/07/Italian-Mozzarella-Chicken-21-1024x1536.jpg')
+  ];
 
   constructor() { }
 
